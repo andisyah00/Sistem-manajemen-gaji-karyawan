7d6597fdb48fc3c01bd6b5faa48a308eeb1f0dee
andisyah00 <77496362+andisyah00@users.noreply.github.com>
2021-01-15 23:36:39 +0700

    Add files via upload

---
+++ src/DataData/DataJabatan.java
@@ -0,0 +1,7 @@
+package DataData;
+
+
+public class DataJabatan {
+      public static String jabatan [] = {"Karyawan", "Staff", "Cleaning Service", "Accounting", "Satpam", "Admin Gudang"};
+      public static String gaji [] = {"3.000.000", "3.500.000", "2.800.000", "4.000.000", "3.400.000", "3.750.000"};
+}
